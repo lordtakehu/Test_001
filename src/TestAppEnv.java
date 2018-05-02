5b3e70e614a7a964f812dc6c6e7e1137d45d3cc4
fedora <fedora@localhost>
2018-05-02 14:11:02 +0700

    ...

---
+++ src/TestAppEnv.java
@@ -0,0 +1,9 @@
+
+public class TestAppEnv {
+
+	public static Object getUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/Test_002.java
+++ src/Test_002.java
@@ -1,12 +1,13 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test_002 {
 	@Test
 	public void checkTitle(){
 		FirefoxDriver driver = new FirefoxDriver();
-		driver.get = ("http://firefox.com");
+		driver.get = (TestAppEnv.getUrl());
 		assertEquals("Title should matched","Firefox",driver.getTitle());
 	}
 
