95817190914422d2816f6d6e4db4321e9b39f492
fedora <fedora@fedora-020021>
2018-05-08 14:43:52 +0700

    ..

--- src/javaTest/TestAppEnv.java
+++ src/javaTest/TestAppEnv.java
@@ -5,13 +5,18 @@ public class TestAppEnv {
 
 	private static Scanner scan;
 
-	public static Object getUrl() {
+	public static String getUrl() {
 		scan = new Scanner(System.in);
         String num = "0";
-        //int sum = 0;
-
         System.out.println("Please enter a link");
         num = scan.nextLine();
         return "http://"+num;
+	}
+
+	public static String getKey() {
+		scan = new Scanner(System.in);
+        String num = "0";
+        System.out.println("Please enter a search text");
+        return num = scan.nextLine();
 	}
 }
--- src/javaTest/Test_002.java
+++ src/javaTest/Test_002.java
@@ -3,15 +3,22 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.firefox.*;
 
 public class Test_002 {
 	@Test
-	public void checkTitle(){
+	public void checkTitle() {
 		System.setProperty("webdriver.gecko.driver","/home/fedora/Downloads/eclipse/geckodriver");
 		System.setProperty("webdriver.firefox.bin","/usr/bin/firefox");
 		WebDriver driver = new FirefoxDriver();
-		driver.get ((String) TestAppEnv.getUrl());
-		assertEquals("Title should matched","Download Firefox — Free Web Browser",driver.getTitle());
+		String link = TestAppEnv.getUrl();
+		driver.get (link);
+		WebElement a = driver.findElement(By.id("search_form_input_homepage"));
+		String key=TestAppEnv.getKey();
+		a.sendKeys(key+Keys.RETURN);
+		assertEquals("Title should matched",key+" at "+link,driver.getTitle());
+		driver.close();
 	}
 }
