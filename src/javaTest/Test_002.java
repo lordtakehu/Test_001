95d594cca4467440ac018c3a88e24191848074cf
fedora <fedora@fedora-020021>
2018-05-11 09:31:35 +0700

    dd

--- src/javaTest/Test_002.java
+++ src/javaTest/Test_002.java
@@ -11,14 +11,14 @@ public class Test_002 {
 	@Test
 	public void checkTitle() {
 		System.setProperty("webdriver.gecko.driver",
-		//"/home/fedora/Downloads/eclipse/geckodriver");
-		"/Volumes/USB/wd_usb/Softwares/Office/eclipse-installer/geckodriver_macos");
+		"/media/Softwares/Office/eclipse-installer/geckodriver_linux");
+		//"/Volumes/USB/wd_usb/Softwares/Office/eclipse-installer/geckodriver_macos");
 		System.setProperty("webdriver.firefox.bin",
-		//"/usr/bin/firefox");
-		"/Volumes/Data 240/Applications/Firefox.app/Contents/MacOS/firefox-bin");
+		"/usr/bin/firefox");
+		//"/Volumes/Data 240/Applications/Firefox.app/Contents/MacOS/firefox-bin");
 		WebDriver driver = new FirefoxDriver();
 		String link = TestAppEnv.getUrl();
-		driver.get (link);
+		driver.get ("http://"+link);
 		WebElement a = driver.findElement(By.id("search_form_input_homepage"));
 		String key=TestAppEnv.getKey();
 		a.sendKeys(key+Keys.RETURN);
